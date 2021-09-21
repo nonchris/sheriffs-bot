04f4afbe8bbefcb0bde658f7e65fb5f7eba72cdc
nonchris <git@chris-ge.de>
2021-09-21 23:30:56 +0200

    Add SQLAlchemy to requirements and update description

--- setup.py
+++ setup.py
@@ -8,25 +8,25 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     # TODO: Adjust your project information here
-    name='discord-bot',
+    name='sheriffs-bot',
     version='2.0.0',
-    description='A discord bot template',
+    description='A slim bot for sheriff crazy',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/nonchris/sheriff-bot',
+    url='https://github.com/nonchris/sheriffs-bot',
     author='nonchris',
     author_email='info@nonchris.eu',
 
     project_urls={
-        'Bug Reports': 'https://github.com/nonchris/sheriff-bot/issues',
-        'Source': 'https://github.com/https://github.com/nonchris/sheriff-bot',
+        'Bug Reports': 'https://github.com/nonchris/sheriffs-bot/issues',
+        'Source': 'https://github.com/https://github.com/nonchris/sheriffs-bot',
     },
 
     keywords='discord-bot',
 
     python_requires='>=3.8, <4',
 
-    install_requires='discord.py ~= 1.7.2',
+    install_requires=['discord.py ~= 1.7.2', 'SQLAlchemy ~= 1.4.15'],
 
     classifiers=[
 
@@ -53,7 +53,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'sheriff-bot=bot:main',
+            'sheriffs-bot=bot:main',
         ],
     },
 )
